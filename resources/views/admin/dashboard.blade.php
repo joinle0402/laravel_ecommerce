50c529d2512f65e242f1a03e24193215b5955c47
John Smith <nhoxhero073@gmail.com>
2024-03-27 07:29:17 -0700

    add admin dashboard adminlte

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.layouts.index')
+
+@section('content')
+    <h5>Hello Admin Dashboard</h5>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Admin\IndexController;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/login');
+Route::redirect('/', '/admin/dashboard');
 Route::get('/login', [AuthController::class, 'loginView']);
 Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+
+Route::get('/admin/dashboard', [IndexController::class, 'dashboard']);
