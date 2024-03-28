93890c23bff628387b2a3f2af20f64c0ede9111d
John Smith <nhoxhero073@gmail.com>
2024-03-28 05:52:39 -0700

    update sidebar

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -6,15 +6,13 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Users\StoreUserRequest;
 use App\Http\Requests\Admin\Users\UpdateUserRequest;
 use App\Models\User;
+use Illuminate\Contracts\View\View;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function index(): View
     {
-        //
+        return view('admin.pages.users.index');
     }
 
     /**
--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -18,28 +18,27 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-item">
                     <a href="{{ route("admin.dashboard") }}" @class(['nav-link', 'active' => request()->routeIs('admin.dashboard')])>
-                        <i class="fas fa-grip-vertical"></i>
+                        <i class="nav-icon fas fa-th"></i>
                         <p style="margin-left: 0.5rem">Dashboard</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{ route("admin.users.index") }}" @class(['nav-link', 'active' => request()->routeIs('admin.users.*')])>
-                        <i class="fas fa-users"></i>
-                        <p style="margin-left: 0.5rem">User</p>
+                        <i class="fas fa-users" style="margin-left: 2px;"></i>
+                        <p style="margin-left: 14px;">User</p>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <form action="{{ route('auth.logout') }}" method="POST">
+                    <a href="javascript:void(0)" onclick="$('#logout-form').submit();" class="nav-link">
+                        <i class="fas fa-sign-out-alt" style="margin-left: 4px;"></i>
+                        <p style="margin-left: 15px;">Logout</p>
+                    </a>
+                    <form action="{{ route('auth.logout') }}" method="POST" id="logout-form">
                         @csrf
                         @method('POST')
-                        <button type="submit" class="nav-link">
-                            <i class="fas fa-sign-out-alt"></i>
-                            <p style="margin-left: 0.5rem">Logout</p>
-                        </button>
                     </form>
                 </li>
             </ul>
         </nav>
     </div>
-
 </aside>
